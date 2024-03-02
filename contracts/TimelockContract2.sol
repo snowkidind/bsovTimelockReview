6fe4b1ee3c7ddb3ee749c918e8bc314c4d01d7c1
snowkidind <keny@eastcoastbands.com>
2024-03-02 18:49:11 +0700

    fix whitespace timelock contract

--- contracts/TimelockContract2.sol
+++ contracts/TimelockContract2.sol
@@ -54,17 +54,17 @@ interface ITimelockRewardReserve {
     function updateEligibility(address user, uint256 amount) external;
 }
 
-
 contract TimelockContract2 {
+  
     address owner;
     using SafeMath for uint;
 
     struct incomingTimelockStruct {
-    uint256 amount;
-    uint256 newLockTime;
-    address from;
-    bool isPending;
-}
+      uint256 amount;
+      uint256 newLockTime;
+      address from;
+      bool isPending;
+    }
 
     address public tokenContract;
     address public timelockRewardReserveAddress;
@@ -77,8 +77,8 @@ contract TimelockContract2 {
     uint unfreezeDate;
     mapping (address => uint) incomingAccountBalance;
     mapping (address => uint) incomingAccountLockExpiration;
-	mapping (address => uint) balance;
-	mapping (address => uint) lastWithdrawal;
+	  mapping (address => uint) balance;
+	  mapping (address => uint) lastWithdrawal;
     mapping (address => uint) lastIncomingAccountWithdrawal;
     mapping(address => incomingTimelockStruct) public pendingIncoming;
     event sendTimelockedMarked(address indexed from, address indexed to, uint256 amount);
@@ -88,13 +88,13 @@ contract TimelockContract2 {
         address indexed addr,
         uint256 amt,
         uint256 time
-	);
+	  );
 
     event TokensUnfrozen (
         address indexed addr,
         uint256 amt,
         uint256 time
-	);
+	  );
 
     constructor( address _tokenContract ) public {
       tokenContract = _tokenContract;
@@ -107,113 +107,113 @@ contract TimelockContract2 {
         _;
     }
 
-// This function sets the Timelock Reward Reserve contract's address. Can only be done by owner.
-function setTimelockRewardReserveAddress(address _address) public onlyOwner {
-    timelockRewardReserveAddress = _address;
-    timelockRewardReserve = ITimelockRewardReserve(timelockRewardReserveAddress);
-}
-
-// This function sends timelocked tokens to one or several addresses' "Untaken Incoming Balance".
-function markTimelockedTokensForSend(address[] memory _receivers, uint[] memory _amounts) public {
-    require(_receivers.length == _amounts.length, "Mismatched array lengths");
-
-    uint totalAmount = 0;
-    for (uint i = 0; i < _amounts.length; i++) {
-        totalAmount += _amounts[i];
+    // This function sets the Timelock Reward Reserve contract's address. Can only be done by owner.
+    function setTimelockRewardReserveAddress(address _address) public onlyOwner {
+        timelockRewardReserveAddress = _address;
+        timelockRewardReserve = ITimelockRewardReserve(timelockRewardReserveAddress);
+    }
+    
+    // This function sends timelocked tokens to one or several addresses' "Untaken Incoming Balance".
+    function markTimelockedTokensForSend(address[] memory _receivers, uint[] memory _amounts) public {
+        require(_receivers.length == _amounts.length, "Mismatched array lengths");
+    
+        uint totalAmount = 0;
+        for (uint i = 0; i < _amounts.length; i++) {
+            totalAmount += _amounts[i];
+        }
+    
+        require(balance[msg.sender] >= totalAmount, "Insufficient timelocked balance. You have to timelock tokens before sending timelocked tokens.");
+    
+        for (uint i = 0; i < _receivers.length; i++) {
+            address receiver = _receivers[i];
+            uint amount = _amounts[i];
+            
+            pendingIncoming[receiver].amount += amount;
+            pendingIncoming[receiver].from = msg.sender;
+            pendingIncoming[receiver].isPending = true;
+    
+            emit sendTimelockedMarked(msg.sender, receiver, amount);
+        }
+        balance[msg.sender] -= totalAmount;
     }
 
-    require(balance[msg.sender] >= totalAmount, "Insufficient timelocked balance. You have to timelock tokens before sending timelocked tokens.");
-
-    for (uint i = 0; i < _receivers.length; i++) {
-        address receiver = _receivers[i];
-        uint amount = _amounts[i];
-        
-        pendingIncoming[receiver].amount += amount;
-        pendingIncoming[receiver].from = msg.sender;
-        pendingIncoming[receiver].isPending = true;
-
-        emit sendTimelockedMarked(msg.sender, receiver, amount);
-    }
-    balance[msg.sender] -= totalAmount;
-}
-
-// This function accepts Untaken Incoming Balance - the Lock Time will reset for the Incoming Tokens Account.
-function acceptIncomingTokens() public {
-    require(pendingIncoming[msg.sender].isPending, "You have no Incoming Tokens to accept!");
-    incomingTimelockStruct memory incomingTokens = pendingIncoming[msg.sender];
-    incomingAccountBalance[msg.sender] += incomingTokens.amount; 
-    incomingAccountLockExpiration[msg.sender] = now + resetIncomingAccountTimeLeft; // When Incoming Tokens are accepted, the unlock date resets to the days specified in resetIncomingAccountTimeLeft.
-    delete pendingIncoming[msg.sender]; 
-    emit TokensClaimed(msg.sender, incomingTokens.amount);
-}
-
-// This function withdraws timelocked tokens once the Lock Time has expired. You can choose which account you want to withdraw from.
-function withdraw(uint _amount, bool fromIncomingTokensAccount) public {
-    require(_amount > 0, "Withdraw amount must be greater than zero");
-    uint256 balanceToCheck = fromIncomingTokensAccount ? incomingAccountBalance[msg.sender] : balance[msg.sender];
-    uint256 lockExpiration = fromIncomingTokensAccount ? incomingAccountLockExpiration[msg.sender] : unfreezeDate;
-    require(now >= lockExpiration, "Tokens are locked!");
-    require(balanceToCheck >= _amount, "Insufficient timelocked balance for withdrawal");    
-
-    require(_amount <= maxWithdrawalAmount, "Exceeds max withdrawal amount");
-
-    if (fromIncomingTokensAccount) {
-        require(now >= lastIncomingAccountWithdrawal[msg.sender] + timeBetweenWithdrawals, "Trying to withdraw too frequently!");
-        incomingAccountBalance[msg.sender] -= _amount;
-        lastIncomingAccountWithdrawal[msg.sender] = now;
-    } else {
-        require(now >= lastWithdrawal[msg.sender] + timeBetweenWithdrawals, "Trying to withdraw too frequently!");
-        balance[msg.sender] -= _amount;
-        lastWithdrawal[msg.sender] = now;
+    // This function accepts Untaken Incoming Balance - the Lock Time will reset for the Incoming Tokens Account.
+    function acceptIncomingTokens() public {
+        require(pendingIncoming[msg.sender].isPending, "You have no Incoming Tokens to accept!");
+        incomingTimelockStruct memory incomingTokens = pendingIncoming[msg.sender];
+        incomingAccountBalance[msg.sender] += incomingTokens.amount; 
+        incomingAccountLockExpiration[msg.sender] = now + resetIncomingAccountTimeLeft; // When Incoming Tokens are accepted, the unlock date resets to the days specified in resetIncomingAccountTimeLeft.
+        delete pendingIncoming[msg.sender]; 
+        emit TokensClaimed(msg.sender, incomingTokens.amount);
     }
 
-    require(ERC20Interface(tokenContract).transfer(msg.sender, _amount), "Withdrawal: Transfer failed");
-    emit TokensUnfrozen(msg.sender, _amount, now);
-}
+    // This function withdraws timelocked tokens once the Lock Time has expired. You can choose which account you want to withdraw from.
+    function withdraw(uint _amount, bool fromIncomingTokensAccount) public {
+        require(_amount > 0, "Withdraw amount must be greater than zero");
+        uint256 balanceToCheck = fromIncomingTokensAccount ? incomingAccountBalance[msg.sender] : balance[msg.sender];
+        uint256 lockExpiration = fromIncomingTokensAccount ? incomingAccountLockExpiration[msg.sender] : unfreezeDate;
+        require(now >= lockExpiration, "Tokens are locked!");
+        require(balanceToCheck >= _amount, "Insufficient timelocked balance for withdrawal");    
+        require(_amount <= maxWithdrawalAmount, "Exceeds max withdrawal amount");
+    
+        if (fromIncomingTokensAccount) {
+            require(now >= lastIncomingAccountWithdrawal[msg.sender] + timeBetweenWithdrawals, "Trying to withdraw too frequently!");
+            incomingAccountBalance[msg.sender] -= _amount;
+            lastIncomingAccountWithdrawal[msg.sender] = now;
+        } else {
+            require(now >= lastWithdrawal[msg.sender] + timeBetweenWithdrawals, "Trying to withdraw too frequently!");
+            balance[msg.sender] -= _amount;
+            lastWithdrawal[msg.sender] = now;
+        }
+    
+        require(ERC20Interface(tokenContract).transfer(msg.sender, _amount), "Withdrawal: Transfer failed");
+        emit TokensUnfrozen(msg.sender, _amount, now);
+    }
 
-// This function gets the balance of the regular account.
+    // This function gets the balance of the regular account.
     function getBalance(address _addr) public view returns (uint256 _balance) {
         return balance[_addr];
     }
 
-// This function gets the time (seconds) since the last withdrawal from the Incoming Tokens Account
-function getLastIncomingAccountWithdrawal(address _addr) public view returns (uint256 _lastWithdrawalTime) {
-    return lastIncomingAccountWithdrawal[_addr];
-}
+    // This function gets the time (seconds) since the last withdrawal from the Incoming Tokens Account
+    function getLastIncomingAccountWithdrawal(address _addr) public view returns (uint256 _lastWithdrawalTime) {
+        return lastIncomingAccountWithdrawal[_addr];
+    }
 
-// This function gets the time (in seconds) since the last withdrawal from the Regular Account
+    // This function gets the time (in seconds) since the last withdrawal from the Regular Account
     function getLastWithdrawal(address _addr) public view returns (uint256 _lastWithdrawal) {
         return lastWithdrawal[_addr];
     }
    
-// This function gets the time left (in seconds) of the Lock Time of the Regular Account   
+    // This function gets the time left (in seconds) of the Lock Time of the Regular Account   
     function getTimeLeft() public view returns (uint256 _timeLeft) {
         require(unfreezeDate > now, "Tokens are unlocked and ready for withdrawal");
         return unfreezeDate - now;
     } 
-// This function gets the time left (in seconds) of the Lock Time of the Incoming Tokens Account
+
+    // This function gets the time left (in seconds) of the Lock Time of the Incoming Tokens Account
     function getIncomingAccountTimeLeft(address _addr) public view returns (uint256 _timeLeft) {
-    if (incomingAccountLockExpiration[_addr] <= now) {
-        return 0; // Tokens are already unlocked
-    } else {
-        return incomingAccountLockExpiration[_addr] - now;
+        if (incomingAccountLockExpiration[_addr] <= now) {
+            return 0; // Tokens are already unlocked
+        } else {
+            return incomingAccountLockExpiration[_addr] - now;
+        }
     }
-}
 
-// This function gets the balance of the Untaken Incoming Tokens
-function getUntakenIncomingBalance(address _user) public view returns (uint256) {
-    if(pendingIncoming[_user].isPending) {
-        return pendingIncoming[_user].amount;
-    } else {
-        return 0;
+    // This function gets the balance of the Untaken Incoming Tokens
+    function getUntakenIncomingBalance(address _user) public view returns (uint256) {
+        if(pendingIncoming[_user].isPending) {
+            return pendingIncoming[_user].amount;
+        } else {
+            return 0;
+        }
     }
-}
-
-// This function gets the balance of the Incoming Tokens Account
-function getIncomingAccountBalance(address _addr) public view returns (uint256 _balance) {
-    return incomingAccountBalance[_addr];
-}
-
+    
+    // This function gets the balance of the Incoming Tokens Account
+    function getIncomingAccountBalance(address _addr) public view returns (uint256 _balance) {
+        return incomingAccountBalance[_addr];
+    }
+    
     function receiveApproval(address _sender, uint256 _value, address _tokenContract, bytes memory ) public {
         require(_tokenContract == tokenContract, "Can only deposit BSOV into this contract!");
         require(_value > 100, "Value must be greater than 100 Mundos, (0.00000100 BSOV)");
@@ -223,10 +223,9 @@ function getIncomingAccountBalance(address _addr) public view returns (uint256 _
         balance[_sender] += _adjustedValue;
         emit TokensFrozen(_sender, _adjustedValue, now);
         if (_sender != timelockRewardReserveAddress) {
-        if (timelockRewardReserveAddress != address(0)) {
-        timelockRewardReserve.updateEligibility(_sender, _adjustedValue);
-     }
-  }
-    }
-    
+            if (timelockRewardReserveAddress != address(0)) {
+                timelockRewardReserve.updateEligibility(_sender, _adjustedValue);
+            }
+        }
+    }  
 }
